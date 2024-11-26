c590a1be33a574566af5f17bcf99dd862324b256
andria1828 <andriakobakhidze615@gmail.com>
2024-11-26 23:33:23 +0400

    second commit

--- day 32/classwork/cw.py
+++ day 32/classwork/cw.py
@@ -1,1 +1,13 @@
-# no cw
+def duplicate_count(txt):
+    txt = txt.lower()
+    cc = {}
+    for i in txt:
+        if i in cc:
+            cc[i] += 1
+        else:
+            cc[i] = 1
+    d = 0
+    for ccc in cc.values():
+        if ccc > 1:
+            d += 1 
+    return d
