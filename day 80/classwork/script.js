30bc7223697e34f94c87fddb4cdbe209c25b6273
andria1828 <andriakobakhidze615@gmail.com>
2024-12-22 19:42:57 +0400

    second commit

---
+++ day 80/classwork/script.js
@@ -0,0 +1,21 @@
+let x = 8;
+let o = 1;
+let name = "andria";
+let surname = "kobakhidze";
+let age = 14;
+let num = 2+1;
+alert(x + o)
+alert(name)
+alert(surname)
+alert(age)
+alert(num)
+
+// 2
+
+const num1 = 5;
+const num2 = 7;
+
+console.log("მიმატება",num1 + num2)
+console.log("გამოკლება",num1 - num2)
+console.log("გამრავლება",num1 * num2)
+console.log("გაყოფა",num1 / num2)
